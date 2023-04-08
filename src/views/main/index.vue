82b48bba1911abd6827f7d0c86705f4fb7d2f3b2
Ramazan <ramazan@bimiboo.com>
2023-04-08 17:31:51 +0600

    create main page component

---
+++ src/views/main/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main-container">
+      <Header />
+  </div>
+</template>
+
+<script>
+import Header from '../../components/Header';
+export default {
+	components: {Header}
+};
+</script>
+
+<style lang="scss">
+.main-container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+</style>
