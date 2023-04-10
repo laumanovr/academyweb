df4dc72c1c2009ef43794a4a6c8172fd2cf9113c
Ramazan <ramazan@bimiboo.com>
2023-04-10 17:22:13 +0600

    moved header to global

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <div id="app">
+  <div id="app" class="main-container">
+    <Header />
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from '../src/components/Header';
+export default {
+	components: {
+		Header
+	}
+};
+</script>
+
+<style lang="scss">
+.main-container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+</style>
--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -1,24 +1,14 @@
 <template>
-  <div class="main-container">
-      <Header />
+  <div class="main-content">
       <Learning />
   </div>
 </template>
 
 <script>
-import Header from '../../components/Header';
 import Learning from '../../components/Learning';
 export default {
 	components: {
-	  Header,
 		Learning
 	}
 };
 </script>
-
-<style lang="scss">
-.main-container {
-  max-width: 1440px;
-  margin: 0 auto;
-}
-</style>
