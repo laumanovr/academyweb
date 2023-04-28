bf3b363404fa42370990e4c1060918c3e3df86f4
Ramazan <ramazan@bimiboo.com>
2023-04-28 10:06:05 +0600

    made form-block in contacts

--- src/views/contacts/index.vue
+++ src/views/contacts/index.vue
@@ -4,17 +4,17 @@
       <Breadcrumbs :items="breadcrumbLinks" />
       <div class="contacts__info-blocks">
         <div class="contacts__info-block">
-          <h3 class="contacts__info-title">General inquiries <br> & comments</h3>
+          <h3 class="contacts__info-title h3">General inquiries <br> & comments</h3>
           <a href="#" class="contacts__info-link">info@bimiboo.com</a>
         </div>
         <div class="contacts__info-block">
-          <h3 class="contacts__info-title">Post mail</h3>
+          <h3 class="contacts__info-title h3">Post mail</h3>
           <div class="contacts__info-description">
             Bimi Boo Kids Inc., <br> 410 Hauser Blvd, Apt 4C, Los Angeles, <br> CA, 90036</div>
           <a href="" class="contacts__info-link">View on Google Maps</a>
         </div>
         <div class="contacts__info-block">
-          <h3 class="contacts__info-title">Business opportunities</h3>
+          <h3 class="contacts__info-title h3">Business opportunities</h3>
           <div class="contacts__info-description">For partnerships and new business enquiries:</div>
           <a href="" class="contacts__info-link">dmitry@bimiboo.com</a>
           <div class="contacts__info-description">For partners</div>
@@ -22,16 +22,36 @@
         </div>
       </div>
     </div>
-
-
+    <div class="contacts__form-container">
+      <div class="contacts__form-block">
+        <div class="contacts__form-text-block">
+          <h2 class="contacts__form-title h2">Have some questions?</h2>
+          <div class="contacts__form-hint">We are always ready to answer</div>
+        </div>
+        <form class="contacts__form">
+          <div class="contacts__form-input">
+            <input type="text" placeholder="Name">
+          </div>
+          <div class="contacts__form-input email">
+            <input type="text" placeholder="Email">
+          </div>
+          <div class="contacts__form-textarea">
+            <textarea placeholder="Message"></textarea>
+          </div>
+          <VButton theme="secondary" class="contacts__form-btn">Submit</VButton>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Breadcrumbs from '@/components/Breadcrumbs';
+import Breadcrumbs from '@/components/Breadcrumbs.vue';
+import VButton from '@/components/VButton.vue';
 export default {
 	components: {
-		Breadcrumbs
+		Breadcrumbs,
+    VButton
 	},
 	data() {
 		return {
@@ -49,10 +69,16 @@ export default {
   color: $dark-blue;
   &__info-container {
     background: $ghost-white;
-    height: 505px;
+    height: 465px;
+    @media #{$lg} {
+      height: 705px;
+    }
     @media #{$sm} {
       padding: 20px 20px 0;
-      height: 750px;
+      height: 670px;
+    }
+    @media #{$xs} {
+      height: 752px;
     }
   }
   &__info-blocks {
@@ -106,11 +132,97 @@ export default {
       line-height: 22px;
     }
   }
-  //&__
-  //&__
-  //&__
-  //&__
-  //&__
-  //&__
+  &__form-container {
+    padding: 100px 120px 0;
+    border-radius: 100px 100px 0 0;
+    background: #fff;
+    position: relative;
+    top: -80px;
+    @media #{$lg} {
+      padding: 130px 40px 0;
+    }
+    @media #{$sm} {
+      padding: 100px 20px 0;
+    }
+    @media #{$xs} {
+      padding: 80px 20px 0;
+      border-radius: 50px 50px 0 0;
+    }
+  }
+  &__form-block {
+    display: flex;
+    @media #{$sm} {
+      display: block;
+    }
+  }
+  &__form-text-block {
+    margin-right: 180px;
+    @media #{$lg} {
+      margin-right: 250px;
+    }
+    @media #{$sm} {
+      margin: 0;
+    }
+  }
+  &__form-title {
+    color: $dark-blue;
+    margin-bottom: 30px;
+    max-width: 225px;
+    @media #{$sm} {
+      max-width: 100%;
+      margin-bottom: 10px;
+    }
+  }
+  &__form-hint {
+    color: $cool-gray;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+    @media #{$sm} {
+      margin-bottom: 40px;
+    }
+  }
+  &__form {
+    width: 386px;
+    @media #{$lg} {
+      width: 380px;
+    }
+    @media #{$sm} {
+      width: 100%;
+    }
+  }
+  &__form-input {
+    input {
+      width: 100%;
+      height: 65px;
+      background: $ghost-white;
+      border-radius: 12px;
+      border: 0;
+      outline: none;
+      padding: 20px;
+    }
+    &.email {
+      margin: 30px 0;
+    }
+  }
+  &__form-textarea {
+    textarea {
+      width: 100%;
+      height: 160px;
+      background: $ghost-white;
+      border-radius: 12px;
+      border: 0;
+      padding: 20px;
+      outline: none;
+      resize: none;
+    }
+  }
+  &__form-btn {
+    width: 165px;
+    margin-top: 30px;
+    @media #{$sm} {
+      height: 50px;
+    }
+  }
 }
 </style>
