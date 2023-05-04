a88d2737b97f6d31567ae77227bcf41d38ec2cf4
Ramazan <ramazan@bimiboo.com>
2023-05-04 11:08:14 +0600

    import vue-modal

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VModal from "vue-js-modal";
 import "./assets/styles/main.scss";
 
 Vue.config.productionTip = false;
+Vue.use(VModal);
 
 new Vue({
 	router,
