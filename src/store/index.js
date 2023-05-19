24ababe9d2d5a765a7769ae1e98838183fc58003
Ramazan <ramazan@bimiboo.com>
2023-05-19 11:59:54 +0600

    add loader to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import auth from "./modules/auth";
+import loader from "./modules/loader";
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  modules: {
-    auth,
-  },
+	modules: {
+		auth,
+		loader
+	},
 });
 
 export default store;
