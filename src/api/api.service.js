db232c7ef2b11f5405cf910fb8841a1ecaf4a063
Ramazan <ramazan@bimiboo.com>
2023-05-19 11:58:56 +0600

    create main api service

--- src/api/api.service.js
+++ src/api/api.service.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import {authHeader} from "@/utils/authHeader";
 
+// const BASE_URL = 'https://apiacademy.mybimiboo.dev:9080';
+const BASE_URL = 'http://3.123.232.84:9080';
+
 export const sendGetRequest = (url) => sendRequest('GET', url);
 
 export const sendPostRequest = (url, data) => sendRequest('POST', url, data);
@@ -9,7 +12,8 @@ export const sendPutRequest = (url, data) => sendRequest('PUT', url, data);
 
 export const sendDeleteRequest = (url, data) => sendRequest('DELETE', url, data);
 
-const sendRequest = async (method, url, data={}) => {
+const sendRequest = async (method, finalUrl, data={}) => {
+	const url = `${BASE_URL}/${finalUrl}`;
 	const isFormData = data instanceof FormData;
 	const config = {
 		method,
