3035836c440e3227cd3e9298791490ccb2f04d99
Ramazan <ramazan@bimiboo.com>
2023-05-26 11:26:51 +0600

    add api for create subscrition

--- src/api/subscription.js
+++ src/api/subscription.js
@@ -1,4 +1,4 @@
-import {sendDeleteRequest, sendGetRequest} from '@/api/api.service';
+import {sendDeleteRequest, sendGetRequest, sendPostRequest} from '@/api/api.service';
 
 export class Subscription {
 	constructor () {
@@ -14,8 +14,13 @@ export class Subscription {
 	  return sendGetRequest(url);
 	}
 
+	createSubscription(body) {
+		const url = 'iap/stripe/subscription';
+		return sendPostRequest(url, body);
+	}
+
 	cancelSubscription(subId) {
-	  const url = `/stripe/subscriptions/${subId}`;
+	  const url = `iap/stripe/subscriptions/${subId}`;
 		return sendDeleteRequest(url, '');
 	}
 }
